f557fb77311bb13aeb7cb2ee57f9e86bd048b9f4
Robert Farb <farb.robert@gmail.com>
2018-07-12 10:58:05 -0700

    Create User model with auth methods

--- music/app/models/user.rb
+++ music/app/models/user.rb
@@ -1,3 +1,37 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint(8)        not null, primary key
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ApplicationRecord
-  
+  validates :email, :session_token, presence: true
+  after_initialize :ensure_session_token
+  attr_reader :password
+
+  def self.generate_session_token
+     SecureRandom.urlsafe_base64
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
 end
