9e6d3bb2420d59045389694dfb168c1cd0160e6a
Robert Farb <farb.robert@gmail.com>
2018-07-12 11:29:21 -0700

    Add find_by_credentials to User model

--- music/app/models/user.rb
+++ music/app/models/user.rb
@@ -12,8 +12,14 @@
 
 class User < ApplicationRecord
   validates :email, :session_token, presence: true
+  validates :password, length: {minimum: 6}
   after_initialize :ensure_session_token
   attr_reader :password
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    return user if user.is_password?(password)
+  end
 
   def self.generate_session_token
      SecureRandom.urlsafe_base64
@@ -31,11 +37,16 @@ class User < ApplicationRecord
 
   def password=(password)
     @password = password
-    self.password_digest = BCrypt::Password.create(password)
+    self.password_digest = BCrypt::Passwordss.create(password)
   end
 
   def is_password?(password)
-    BCrypt::Password.new(password_digest).is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
   end
 
+
+  private
+  def user_params
+    params.require(:user).allow(:email, :password)
+  end
 end
